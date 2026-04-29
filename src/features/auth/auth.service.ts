12a6f9e9b1e6150603d07f324966b1d264ebf1a6
Muhammed Hafis <muhammedhafis302@gmail.com>
2026-04-29 21:50:37 +0530

    auth update

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -6,13 +6,13 @@ export const authenticate = async (email: string, password: string) => {
   const user = await findAdminByEmail(email);
 
   if (!user) {
-    throw new Error("Invalid email or password");
+    throw new Error("Invalid credentials");
   }
 
   const isPasswordMatch = await bcrypt.compare(password, user.password);
 
   if (!isPasswordMatch) {
-    throw new Error("Invalid email or password");
+    throw new Error("Invalid credentials");
   }
 
   const accessToken = generateAccessToken({ email: user.email,id:user._id });
